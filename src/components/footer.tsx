52dfc42e2cc92e4dabb056dbcdadca9443bd516c
Geneviève Perron Migneron <info@gen-migneron.com>
2024-06-06 23:32:08 -0400

    Add application buton in footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -36,7 +36,9 @@ const Footer: FC<FooterProps> = ({ data, settings }) => {
             ))
           )}
         </nav> 
-        <Newsletter />
+        <nav>
+          <a href={t('formLink')} target="_blank" className="btn btn-primary">{t('application')}</a>
+        </nav>
       </footer> 
       <footer className="footer px-10 py-4 border-t bg-base-200 text-base-content border-base-300">
         <aside className="items-center grid-flow-col">
