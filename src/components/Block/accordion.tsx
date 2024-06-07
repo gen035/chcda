0d825b4447f50286ae2ee7740621d1041cab11e1
Geneviève Perron Migneron <info@gen-migneron.com>
2024-06-06 23:08:48 -0400

    Fix hero styles

--- src/components/Block/accordion.tsx
+++ src/components/Block/accordion.tsx
@@ -14,10 +14,10 @@ const Accordion: FC<AccordionInterface> = ({ data }) => {
   }
 
   return (
-    <div data-id={data.id} className="max-w-screen-md mx-auto p-4">
+    <div data-id={data.id} className="accordion max-w-screen-md mx-auto p-4">
       {data.title && <h1 className="text-center text-primary mt-4 mb-4">{data.title}</h1>}
       {data.items.map((item: AccordionItem, index: number) => (
-        <div key={index} data-id={item.sys?.id} className="collapse collapse-arrow bg-base-200 mb-2">
+        <div key={index} data-id={item.sys?.id} className="accordion-item collapse collapse-arrow bg-base-200 mb-2">
           <input type="radio" name={`accordion-${data.id}`} defaultChecked />
           <div className="collapse-title text-xl font-medium">
             {item.title}
--- src/components/Block/hero.tsx
+++ src/components/Block/hero.tsx
@@ -22,8 +22,9 @@ const Hero: FC<BlockInterface> = ({ data }) => {
   };
 
   return (
-    <div data-id={data?.id} className={`flex mx-auto mb-8 px-4 py-0 justify-center items-center ${getClasses()}`} style={divStyle}>
-      <div className={`content ${data?.type === 'hero' ? 'w-full md:w-3/4 lg:w-1/2 flex flex-col' : ''}`}>
+    <div data-id={data?.id} className={`flex mx-auto mb-8 px-4 py-0 justify-center items-center relative ${getClasses()}`}>
+      <div className="hero-filter brightness-50 absolute top-0 left-0 w-full h-full" style={divStyle}></div>
+      <div className={`text-white content z-10 ${data?.type === 'hero' ? 'w-full md:w-3/4 lg:w-1/2 flex flex-col' : ''}`}>
         {data?.title && <h1 className='order-2'>{data?.title}</h1>}
         {data?.subtitle && <h2 className='order-1'>{data?.subtitle}</h2>}
         {data?.description && <ReactMarkdown className='order-3'>{data.description}</ReactMarkdown>}
