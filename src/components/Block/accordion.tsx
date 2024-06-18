11f5cfa74124f730ac52bc7333af3e3f5d671740
Geneviève Perron Migneron <info@gen-migneron.com>
2024-06-17 20:46:36 -0400

    Fix accordion opened

--- src/components/Block/accordion.tsx
+++ src/components/Block/accordion.tsx
@@ -13,7 +13,7 @@ const Accordion: FC<AccordionInterface> = ({ data }) => {
       {data.title && <h1 className="text-center text-gradient text-primary mt-4 mb-4">{data.title}</h1>}
       {data.items.map((item: AccordionItemInterface, index: number) => (
         <div key={index} data-id={item.sys?.id} className="accordion-item collapse collapse-arrow bg-base-200 mb-2">
-          <input type="radio" name={`accordion-${data.id}`} defaultChecked />
+          <input type="radio" name={`accordion-${data.id}`} defaultChecked={index === 0} />
           <div className="collapse-title text-xl font-medium">
             {item.title}
           </div>
